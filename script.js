15c7b6cf00c090c15feba5c713b3e179eb3df059
Deborah Campos <631care@gmail.com>
2021-10-14 12:52:12 -0400

    added queryselectors still working on fetch

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-let selectMovie = document.querySelector("#move-select")
+fetch("https://ghibliapi.herokuapp.com/films")
+.then((res)=>{
+    return res.json();
+}).then ((data)=>{
+    let films = data.films
+    console.log(films)
+}).catch((err)=>{
+    consol.log(err)
+});
+
+let selectMovie = document.querySelector("#move-select");
 let errorMsg = document.querySelector("#error-msg");
-let defaultValue = document.querySelector("#default")
+let defaultValue = document.querySelector("#default");
+let movieDescription = document.querySelector("#display-info p");
+let submit = document.querySelector("#submit");
+let reviews = document.querySelector("#reviews ul");
 
-fetch()
+// selectMovie.addEventListener("input", (e)=>{
+//     e.preventDefault();
 
+//     let selectedMovie = e.target["#movie-select"].value 
+//     console.log(selectedMovie)
 
-
-selectMovie.addEventListener("input", (e)=>{
-    e.preventDefault();
-
-    let selectedMovie = e.target["#movie-select"].value 
-    console.log(selectedMovie)
-
-    if (selectedMovie !== defaultValue.value){
-        errorMsg.textContent = "Please select a Movie"
-        console.log(errorMsg)
-    } else {
+//     if (selectedMovie !== defaultValue.value){
+//         errorMsg.textContent = "Please select a Movie"
+//         console.log(errorMsg)
+//     } else {
        
-    }
+//     }
 
-})
+// })
