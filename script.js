f09621d02e0a79aafec0da049451ba3cdbd48353
Deborah Campos <631care@gmail.com>
2021-10-14 13:50:28 -0400

    01 tests pass

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
 fetch("https://ghibliapi.herokuapp.com/films")
 .then((res)=>{
     return res.json();
-}).then ((data)=>{
-    let films = data.films
-    console.log(films)
-}).catch((err)=>{
-    consol.log(err)
+}).then((movies)=>{
+    console.log(movies[0].title);
+    console.log(movies[0].description)
+    let selectMovie = document.querySelector("#movie-select");
+
+    for(let movie of movies){
+        let option = document.createElement("option");
+        option.textContent = movie.title;
+        option.value = movie.title;
+        selectMovie.append(option);
+    }
+    selectMovie.addEventListener("input", (e)=>{
+        e.preventDefault();
+    let newP = document.createElement("p")
+    let h3 = document.createElement("p")
+        console.log(selectMovie.value);
+        console.log(e.target.value)
+        console.log()
+    
+    })
+    
+
+
+}).catch((err)=>{ 
+    console.log(err)
 });
 
-let selectMovie = document.querySelector("#move-select");
+    //     
+    
+    //     let selectedMovie = e.target["#movie-select"].value 
+    //     console.log(selectedMovie)
+    
+
+
 let errorMsg = document.querySelector("#error-msg");
 let defaultValue = document.querySelector("#default");
 let movieDescription = document.querySelector("#display-info p");
 let submit = document.querySelector("#submit");
 let reviews = document.querySelector("#reviews ul");
 
-// selectMovie.addEventListener("input", (e)=>{
-//     e.preventDefault();
-
-//     let selectedMovie = e.target["#movie-select"].value 
-//     console.log(selectedMovie)
-
+// 
 //     if (selectedMovie !== defaultValue.value){
 //         errorMsg.textContent = "Please select a Movie"
 //         console.log(errorMsg)
