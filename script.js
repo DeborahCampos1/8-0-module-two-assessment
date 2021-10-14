da53bc13a0334ffa9df61a580724a41f42578851
Deborah Campos <631care@gmail.com>
2021-10-14 17:38:06 -0400

    final update, 2 tests fail on 02

--- script.js
+++ script.js
@@ -2,16 +2,13 @@ fetch("https://ghibliapi.herokuapp.com/films")
 .then((res)=>{
     return res.json();
 }).then((movies)=>{
-    let movieDescriptions = movies.description
-    // console.log(movies);
-    // console.log(movies[0].release_date)
-    // console.log(movies[0].description)
     let selectMovie = document.querySelector("#movie-select");
+    let movieDescription = document.querySelector("#display-info");
+    let reviews = document.querySelector("#reviews ul");
 
     for(let movie of movies){
         let option = document.createElement("option");
-        // let year = movie.release_date
-        // let description = movie.description
+
         option.textContent = movie.title;
         option.value = movie.title;
         selectMovie.append(option);
@@ -20,10 +17,11 @@ fetch("https://ghibliapi.herokuapp.com/films")
     selectMovie.addEventListener("input", (e)=>{
         e.preventDefault();
         movieDescription.textContent = "";
+
         let released = movies.filter((movie)=>{
             return movie.title === e.target.value
         })
-        console.log(released)
+    
     let year = document.createElement("p")
     let description = document.createElement("p")
     let h3 = document.createElement("h3")
@@ -40,25 +38,23 @@ fetch("https://ghibliapi.herokuapp.com/films")
     let input = document.querySelector("#review-input")
     let errorMsg = document.querySelector("#error-msg");
     let form = document.querySelector("#movie-review");
+
     form.addEventListener("submit", (e)=>{
         e.preventDefault();
         if (!input.value){
             errorMsg.textContent = "Please enter a review"
 
         } else {
-            
-            let strong = document.createElement("strong")
+            let h3 = document.querySelector("h3").textContent
             let list = document.createElement("li");
-            list.textContent = e.target["review-input"].value;
-            reviews.append(strong)
-            strong.append(list)
+            listReview = e.target["review-input"].value;
+            list.innerHTML = `<strong> ${h3} :</strong> ${listReview}`
+            reviews.append(list)
             input.value = ""
             errorMsg.textContent = ""
             
         }
     })
-
-    
 
 
 }).catch((err)=>{ 
@@ -66,8 +62,5 @@ fetch("https://ghibliapi.herokuapp.com/films")
 });
     
 
-let movieDescription = document.querySelector("#display-info");
-let submit = document.querySelector("#submit");
-let reviews = document.querySelector("#reviews ul");
 
 
