d735500187ca48101c8e8787a8cb066a6aac88ef
Deborah Campos <631care@gmail.com>
2021-10-14 15:44:08 -0400

    functional page, li element needs updates

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ fetch("https://ghibliapi.herokuapp.com/films")
     return res.json();
 }).then((movies)=>{
     let movieDescriptions = movies.description
-    console.log(movies);
-    console.log(movies[0].release_date)
-    console.log(movies[0].description)
+    // console.log(movies);
+    // console.log(movies[0].release_date)
+    // console.log(movies[0].description)
     let selectMovie = document.querySelector("#movie-select");
 
     for(let movie of movies){
         let option = document.createElement("option");
+        // let year = movie.release_date
+        // let description = movie.description
         option.textContent = movie.title;
         option.value = movie.title;
         selectMovie.append(option);
@@ -18,35 +20,42 @@ fetch("https://ghibliapi.herokuapp.com/films")
     selectMovie.addEventListener("input", (e)=>{
         e.preventDefault();
         movieDescription.textContent = "";
-
+        let released = movies.filter((movie)=>{
+            return movie.title === e.target.value
+        })
+        console.log(released)
     let year = document.createElement("p")
     let description = document.createElement("p")
     let h3 = document.createElement("h3")
     h3.textContent = e.target.value;
-    // year.textContent = movies.release_date;
-    // description.textContent = movies.description;
+    year.textContent = released[0].release_date;
+    description.textContent = released[0].description;
 
 
     movieDescription.append(h3);
     movieDescription.append(year)
     movieDescription.append(description)
-
-
-        //console.log(selectMovie.value);
-        //console.log(e.target.value)
-        //console.log()
     
     })
+    let input = document.querySelector("#review-input")
+    let errorMsg = document.querySelector("#error-msg");
     let form = document.querySelector("#movie-review");
     form.addEventListener("submit", (e)=>{
         e.preventDefault();
-        let input = document.querySelector("#review-input")
-        let strong = document.createElement("strong")
-        let list = document.createElement("li");
-        list.textContent = e.target["review-input"].value;
-        reviews.append(strong)
-        strong.append(list)
-        input.value = ""
+        if (!input.value){
+            errorMsg.textContent = "Please enter a review"
+
+        } else {
+            
+            let strong = document.createElement("strong")
+            let list = document.createElement("li");
+            list.textContent = e.target["review-input"].value;
+            reviews.append(strong)
+            strong.append(list)
+            input.value = ""
+            errorMsg.textContent = ""
+            
+        }
     })
 
     
@@ -57,17 +66,8 @@ fetch("https://ghibliapi.herokuapp.com/films")
 });
     
 
-let errorMsg = document.querySelector("#error-msg");
 let movieDescription = document.querySelector("#display-info");
 let submit = document.querySelector("#submit");
 let reviews = document.querySelector("#reviews ul");
 
-// 
-//     if (selectedMovie !== defaultValue.value){
-//         errorMsg.textContent = "Please select a Movie"
-//         console.log(errorMsg)
-//     } else {
-       
-//     }
 
-// })
